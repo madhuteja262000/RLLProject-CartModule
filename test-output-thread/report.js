ca1cc8d066726069c34e2d9c9f32efe70cb26f01
madhutejaswara reddy <madhutejaswara reddy@madhu>
2023-12-13 12:59:34 +0530

    second commit

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"3b08dc83-c61b-4988-9898-1709b31921f0","feature":"Validate Cart Functionality on Chiltern Oak Furniture Website","scenario":"Add Products to Cart","start":1702289258059,"group":1,"content":"","tags":"","end":1702289326384,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"8207854d-765c-44b8-b77f-5c344ab42982","feature":"Validate Cart Functionality on Chiltern Oak Furniture Website","scenario":"Add Products to Cart","start":1702372814535,"group":1,"content":"","tags":"","end":1702372877619,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });
